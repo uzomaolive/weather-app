003c2710a250a930628efda7d46f69bf24a4d920
Olive Uzoma <olive.c.uzoma@gmail.com>
2024-07-02 15:21:19 +0100

    feat(weather-app): implement error handling for invalid city input and toggle display
    - Added a check for a 404 response from the weather API to handle invalid city inputs.
    - Display an error message and hide weather information when the city is not found.
    - Toggle visibility of weather information and error messages based on API response.

--- index.js
+++ index.js
@@ -7,31 +7,72 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
 
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    if(response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+        let data = await response.json();
 
-    if(data.weather[0].main === "Clouds") {  
-        weatherIcon.src = "images/clouds.png";
+        // console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    
+        if(data.weather[0].main === "Clouds") {  
+            weatherIcon.src = "images/clouds.png";
+           
+        }
+    
+        else if(data.weather[0].main === "Clear") {
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main === "Rain") {
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main === "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    
+    }
+
+
+    // let data = await response.json();
+
+    // // console.log(data);
+    // document.querySelector(".city").innerHTML = data.name;
+    // document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    // document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    // document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    // if(data.weather[0].main === "Clouds") {  
+    //     weatherIcon.src = "images/clouds.png";
        
-    }
+    // }
 
-    else if(data.weather[0].main === "Clear") {
-        weatherIcon.src = "images/clear.png";
-    }
-    else if(data.weather[0].main === "Rain") {
-        weatherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main === "Drizzle") {
-        weatherIcon.src = "images/drizzle.png";
-    }
-    else if(data.weather[0].main === "Mist") {
-        weatherIcon.src = "images/mist.png";
-    }
+    // else if(data.weather[0].main === "Clear") {
+    //     weatherIcon.src = "images/clear.png";
+    // }
+    // else if(data.weather[0].main === "Rain") {
+    //     weatherIcon.src = "images/rain.png";
+    // }
+    // else if(data.weather[0].main === "Drizzle") {
+    //     weatherIcon.src = "images/drizzle.png";
+    // }
+    // else if(data.weather[0].main === "Mist") {
+    //     weatherIcon.src = "images/mist.png";
+    // }
+
+    // document.querySelector(".weather").style.display = "block";
+
 
 }
 searchBtn.addEventListener("click", () => {
