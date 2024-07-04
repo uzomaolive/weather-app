2150c06012186365a5ee8bdae3135d00d08780a9
Olive Uzoma <olive.c.uzoma@gmail.com>
2024-07-04 16:05:24 +0100

    feat: Remove the API key
    - remove the exposed API keys from the `index.js` file
    - refactor the fetch function to use the new API route

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,3 @@
-const apiKey = "60cad6303e40616a7c0059f4b82022fe";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -11,7 +8,12 @@ async function checkWeather(city) {
     document.querySelector(".weather").style.display = "none";
     document.querySelector(".error").style.display = "none";
 
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const response = await fetch(`/api/check-weather.js?city=${city}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }   
+    });
     loadingSpinner.style.display = "none"; // Hide loading spinner
 
     if(response.status === 404) {
@@ -26,15 +28,15 @@ async function checkWeather(city) {
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
         if(data.weather[0].main === "Clouds") {  
-            weatherIcon.src = "images/clouds.png";
+            weatherIcon.src = "/images/clouds.png";
         } else if(data.weather[0].main === "Clear") {
             weatherIcon.src = "images/clear.png";
         } else if(data.weather[0].main === "Rain") {
-            weatherIcon.src = "images/rain.png";
+            weatherIcon.src = "/images/rain.png";
         } else if(data.weather[0].main === "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
+            weatherIcon.src = "/images/drizzle.png";
         } else if(data.weather[0].main === "Mist") {
-            weatherIcon.src = "images/mist.png";
+            weatherIcon.src = "/images/mist.png";
         }
 
         document.querySelector(".weather").style.display = "block";
